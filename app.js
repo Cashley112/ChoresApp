52b7c774bc960f71c5d5af725c0224bb90e8a462
Connor Ashley <ashleycm@vcu.edu>
2021-05-04 10:34:57 -0400

    added public folder, and middleware for static files

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ db.once('open', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
