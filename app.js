8544f8d8035eec491149dade8b5329b00b0337a7
Connor Ashley <ashleycm@vcu.edu>
2021-05-04 14:56:34 -0400

    Full CRUD for Task and User models, including views and everything. resFUL

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ db.once('open', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
@@ -30,18 +31,74 @@ app.get('/', (req, res) => {
 })
 app.get('/users', async (req, res) => {
     const users = await User.find({})
-    res.render('users', { users });
+    res.render('User/users', { users });
 })
+app.get('/users/new', (req, res) => {
+    res.render('User/new');
+})
+app.post('/users', async (req, res) => {
+    const user = new User(req.body.user);
+    await user.save();
+    res.redirect('/users')
+})
+app.get('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const tasks = await Task.find({});
+    const user = await User.findById(id).populate('assignedTasks');
+    res.render('User/show', { user, tasks });
+})
+app.patch('/users/:id', async (req, res) => {
+    const { id, taskId } = req.params;
+    const user = await User.findById(id);
+    const task = await Task.findOne(req.body);
+    user.assignedTasks.push(task);
+    await user.save();
+    res.redirect(`/users/${ user._id }`)
+})
+app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.redirect('/users')
+});
 
 app.get('/tasks', async (req, res) => {
     const tasks = await Task.find({});
-    res.render('tasks', { tasks });
+    res.render('Task/tasks', { tasks });
 })
+app.get('/tasks/new', (req, res) => {
+    res.render('Task/new');
+})
+app.post('/tasks', async (req, res) => {
+    task = new Task(req.body.task);
+    await task.save();
+    res.redirect(`/tasks/${ task._id }`)
+})
+app.get('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('Task/show', { task })
+})
+app.get('/tasks/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('Task/edit', { task });
+})
+app.put('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate(id, {...req.body.task});
+    res.redirect(`/tasks/${ task._id }`);
+})
+app.delete('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    res.redirect('/tasks');
+})
+
 app.get('/schedule', async (req, res) => {
     const assignments = await User.find({}).populate('assignedTasks');
     res.render('schedule', { assignments });
 })
-
+// Complete task route
 app.patch('/users/:id/tasks/:taskId', async (req, res) => {
     const { id, taskId } = req.params;
     const updatedTask = await User.findByIdAndUpdate(id, { $pull: { assignedTasks: taskId } });
